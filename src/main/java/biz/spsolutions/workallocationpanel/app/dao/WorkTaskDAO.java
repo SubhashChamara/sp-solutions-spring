4835081bb8a4e4def8b8fe6cf1e15dc5174c071d
Subhash Chamara <udayanga.subhash94@gmail.com>
2023-10-11 15:07:00 +0530

    Pending User Search feature is implemented

--- src/main/java/biz/spsolutions/workallocationpanel/app/dao/WorkTaskDAO.java
+++ src/main/java/biz/spsolutions/workallocationpanel/app/dao/WorkTaskDAO.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @Repository
 public interface WorkTaskDAO {
-    public List<WorkTask> getAll(String wfRef, Date startDate, Date endDate, Integer clientId, Integer projectId, String overallStatus, String developerId, String workType, String chargeType, BigInteger jobType,String userId,String pendingUser) throws Exception;
+    public List<WorkTask> getAll(String wfRef, Date startDate, Date endDate, Integer clientId, Integer projectId, String overallStatus, String developerId, String workType, String chargeType, BigInteger jobType, String userId, String pendingUser) throws Exception;
+
 
 }
